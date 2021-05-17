7b95768d5616746bf2b067a45e35b4b89f552406
Aman Joshi <aman.joshi@crownstack.com>
2021-05-17 13:21:18 +0530

    Initial Commit

---
+++ SwiftUI-Architecture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Architecture
+//
+//  Created by Mohd Maruf on 17/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Architecture/SwiftUI_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ArchitectureApp.swift
+//  SwiftUI-Architecture
+//
+//  Created by Mohd Maruf on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
