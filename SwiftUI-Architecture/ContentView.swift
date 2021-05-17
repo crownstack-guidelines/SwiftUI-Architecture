e03f77161827e74a09015995cbfa04dee5a80cf9
Aman Joshi <aman.joshi@crownstack.com>
2021-05-17 17:41:24 +0530

    Added theme managing files

--- SwiftUI-Architecture/ContentView.swift
+++ SwiftUI-Architecture/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+      Text("Hello, world!").foregroundColor(Color.Token.inactive)
             .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+      ContentView()
+        .environment(\.font, Font.Typography.subFont)
+        .environment(\.colorScheme, .light)
     }
 }
